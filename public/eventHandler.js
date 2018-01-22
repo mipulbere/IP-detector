8af1fbb89715cbad51ef1a536c29ed2c49cecb33
Mirela Pulbere <mirelapulbere@Mirelas-MacBook-Pro.local>
2018-01-22 18:54:44 -0500

    latest updates

--- public/eventHandler.js
+++ public/eventHandler.js
@@ -1,10 +1,10 @@
 function loadDoc(id) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        id.innerHTML = xhttp.responseText;
+      if (this.readyState != 4 || this.status != 200) {
+        console.error("IP logging request failed");
       }
     };
-    xhttp.open("GET",'http://localhost:3000/ip', true);
+    xhttp.open("GET",'/ip', true);
     xhttp.send();
   }
--- response.js
+++ response.js
@@ -4,8 +4,41 @@ var app = express();
 
 app.use(express.static('public'))
 
+var ipMap = {};
 
 app.get('/ip', function (req, res) {
-res.writeHead(200, {'Content-Type': 'text/html'});
-res.end('the ip address is - ' + req.ip);
-}).listen(3000);
+  console.log("/ip received");
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var ip = req.ip;
+  // inside the req handler
+  if (!ipMap[ip]) {
+    console.log("new ip " + ip);
+    var ipStat = {
+      reqCount: 1,
+      lastReqTime: Date.now()
+    };
+    ipMap[ip] = ipStat;
+    console.log("ipStat " + JSON.stringify(ipMap[ip]));
+  } else {
+    ipMap[ip].lastReqTime = Date.now();
+    ipMap[ip].reqCount++;
+  }
+  console.log("ipStat for ip " + ip + " " + JSON.stringify(ipMap[ip]));
+  res.end();
+});
+
+app.get('/admin', function (req, res) {
+  console.log("/admin recieved");
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("<!DOCTYPE html><html><body><table>");
+
+  Object.keys(ipMap).forEach(function(key, index) {
+      res.write(key + " " + JSON.stringify(ipMap[key]) + "<br/>");
+
+  });
+  res.write("</table></body></html>");
+
+  res.end();
+});
+
+app.listen(3000);
